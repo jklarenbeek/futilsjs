12effb4d0205100bb76c977ffd3cf824be659b77
Joham <jklarenbeek@gmail.com>
2019-04-11 14:31:11 +0200

    code cleanup

--- src/vec2i.js
+++ src/vec2i.js
@@ -232,69 +232,68 @@ export function vec2i_triangleIntersect(v1, v2, v3, r1, r2) {
     This function borrowed faithfully from a wonderfl (:3) discussion on
     calculating triangle collision with AABBs on the following blog:
     http://sebleedelisle.com/2009/05/super-fast-trianglerectangle-intersection-test/
-  
+
     This particular optimization best suits my purposes and was contributed
     to the discussion by someone from http://lab9.fr/
-    */
-  
-    const l = r1.x|0;
-    const r = r2.x|0;
-    const t = r1.y|0;
-    const b = r2.y|0;
-  
-    const x0 = v1.x|0;
-    const y0 = v1.y|0;
-    const x1 = v2.x|0;
-    const y1 = v2.y|0;
-    const x2 = v3.x|0;
-    const y2 = v3.y|0;
-  
-    const b0 = (((x0 > l) ? 1 : 0) | (((y0 > t) ? 1 : 0) << 1) |
-        (((x0 > r) ? 1 : 0) << 2) | (((y0 > b) ? 1 : 0) << 3))|0;
-    if (b0 == 3) return true;
-  
-    const b1 = ((x1 > l) ? 1 : 0) | (((y1 > t) ? 1 : 0) << 1) |
-        (((x1 > r) ? 1 : 0) << 2) | (((y1 > b) ? 1 : 0) << 3);
-    if (b1 == 3) return true;
-  
-    const b2 = ((x2 > l) ? 1 : 0) | (((y2 > t) ? 1 : 0) << 1) |
-        (((x2 > r) ? 1 : 0) << 2) | (((y2 > b) ? 1 : 0) << 3);
-    if (b2 == 3) return true;
-  
-    let c = 0;
-    let m = 0;
-    let s = 0;
-  
-    const i0 = (b0 ^ b1)|0;
-    if (i0 != 0) {
-        m = ((y1-y0) / (x1-x0))|0;
-        c = (y0 -(m * x0))|0;
-        if (i0 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
-        if (i0 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
-        if (i0 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
-        if (i0 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
-    }
-  
-    const i1 = (b1 ^ b2)|0;
-    if (i1 != 0) {
-        m = ((y2 - y1) / (x2 - x1))|0;
-        c = (y1 -(m * x1))|0;
-        if (i1 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
-        if (i1 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
-        if (i1 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
-        if (i1 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
-    }
-  
-    const i2 = (b0 ^ b2)|0;
-    if (i2 != 0)
-    {
-        m = ((y2 - y0) / (x2 - x0))|0;
-        c = (y0 -(m * x0))|0;
-        if (i2 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
-        if (i2 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
-        if (i2 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
-        if (i2 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
-    }
-  
-    return false;
+  */
+
+  const x0 = v1.x|0;
+  const y0 = v1.y|0;
+  const x1 = v2.x|0;
+  const y1 = v2.y|0;
+  const x2 = v3.x|0;
+  const y2 = v3.y|0;
+
+  const l = r1.x|0;
+  const r = r2.x|0;
+  const t = r1.y|0;
+  const b = r2.y|0;
+
+  const b0 = (((x0 > l) ? 1 : 0) | (((y0 > t) ? 1 : 0) << 1) |
+      (((x0 > r) ? 1 : 0) << 2) | (((y0 > b) ? 1 : 0) << 3))|0;
+  if (b0 == 3) return true;
+
+  const b1 = ((x1 > l) ? 1 : 0) | (((y1 > t) ? 1 : 0) << 1) |
+      (((x1 > r) ? 1 : 0) << 2) | (((y1 > b) ? 1 : 0) << 3);
+  if (b1 == 3) return true;
+
+  const b2 = ((x2 > l) ? 1 : 0) | (((y2 > t) ? 1 : 0) << 1) |
+      (((x2 > r) ? 1 : 0) << 2) | (((y2 > b) ? 1 : 0) << 3);
+  if (b2 == 3) return true;
+
+  let c = 0;
+  let m = 0;
+  let s = 0;
+
+  const i0 = (b0 ^ b1)|0;
+  if (i0 != 0) {
+    m = ((y1-y0) / (x1-x0))|0;
+    c = (y0 -(m * x0))|0;
+    if (i0 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
+    if (i0 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
+    if (i0 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
+    if (i0 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
   }
+
+  const i1 = (b1 ^ b2)|0;
+  if (i1 != 0) {
+    m = ((y2 - y1) / (x2 - x1))|0;
+    c = (y1 -(m * x1))|0;
+    if (i1 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
+    if (i1 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
+    if (i1 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
+    if (i1 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
+  }
+
+  const i2 = (b0 ^ b2)|0;
+  if (i2 != 0) {
+    m = ((y2 - y0) / (x2 - x0))|0;
+    c = (y0 -(m * x0))|0;
+    if (i2 & 1) { s = m * l + c; if ( s > t && s < b) return true; }
+    if (i2 & 2) { s = (t - c) / m; if ( s > l && s < r) return true; }
+    if (i2 & 4) { s = m * r + c; if ( s > t && s < b) return true; }
+    if (i2 & 8) { s = (b - c) / m; if ( s > l && s < r) return true; }
+  }
+
+  return false;
+}
