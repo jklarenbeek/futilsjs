b733a68efbef7328b6e59cbe917d0eab5d501998
Joham <jklarenbeek@gmail.com>
2019-08-01 23:15:26 +0200

    performance update

--- src/types-base.js
+++ src/types-base.js
@@ -1,6 +1,8 @@
 import { mathi32_round } from './int32-math';
 
 export function isPrimitiveTypeEx(typeString) {
+  // primitives: boolean = 1, integer = 32, float = 64, bigint = 0, letter = 16
+  // complex: struct, array, string, map
   return typeString === 'integer'
     || typeString === 'number'
     || typeString === 'string'
@@ -19,9 +21,103 @@ export function sanitizePrimitiveValue(value, nullable, defaultValue = undefined
   return isPrimitiveType(value) ? value : defaultValue;
 }
 
-export function isPureNumber(obj) {
+// region isPureNumber
+function isPureNumber_asNaN(obj) {
+  // eslint-disable-next-line no-restricted-globals
+  return isNaN(obj) === false;
+}
+
+function isPureNumber_asNumber(obj) {
   return (Number(obj) || false) !== false;
 }
+
+function isPureNumber_asParseInt(obj) {
+  // eslint-disable-next-line radix
+  const n = parseInt(obj);
+  // eslint-disable-next-line no-self-compare
+  return n === n;
+}
+
+function isPureNumber_asParseFloat(obj) {
+  // eslint-disable-next-line radix
+  const n = parseFloat(obj);
+  // eslint-disable-next-line no-self-compare
+  return n === n; // we equal NaN with NaN here.
+}
+
+function isPureNumber_asMathRound(obj) {
+  const n = Math.round(obj);
+  // eslint-disable-next-line no-self-compare
+  return n === n; // we equal NaN with NaN here.
+}
+
+function isPureNumber_asCastFloat(obj) {
+  // eslint-disable-next-line no-self-compare
+  return +obj === +obj; // we equal NaN with NaN here.
+}
+
+// endregion
+
+/**
+ * @example
+ *  const fns_all = [ isPureNumber_asNumber, isPureNumber_asParseFloat ];
+ *  const test_stringi = ['12345', '54321', '12358', '85321'];
+ *  const test_stringf = ['12.345', '54.321', '12.358', '85.321'];
+ *  const test_nan = ['abcde', 'edcba', 'abceh', 'hecba'];
+ *  const test_number = [13.234, 21.123, 34.456, 55.223];
+ *  const test_integer = [13, 21, 34, 55];
+ *  const pref_idx = performanceIndexOfUnaryBool(fns_all, [ test_number, test_integer ]);
+ *  const pref_fn = pref_idx >= 0 ? fns_all[pref_idx] : undefined;
+ *
+ * @param {*} functionList
+ * @param {*} testList
+ */
+export function performanceIndexOfUnaryBool(functionList, testList) {
+  let index = -1;
+  let start = 0.0;
+  let end = 0.0;
+  let delta = 0.0;
+  // eslint-disable-next-line no-unused-vars
+  let tmp = false;
+
+  // eslint-disable-next-line func-names
+  let fn = function () { };
+
+  for (let i = 0; i < functionList.length; ++i) {
+    fn = functionList[i];
+    end = 0.0;
+    for (let j = 0; j < testList.length; ++j) {
+      const test = testList[j];
+      start = performance.now();
+      for (let k = 0; k < 1000; ++k) {
+        tmp |= fn(test % k);
+      }
+      end += performance.now() - start;
+    }
+    end /= testList.length;
+
+    if (delta > end) {
+      delta = end;
+      index = i;
+    }
+  }
+  return index;
+}
+
+export const isPureNumber = (function calibrate(doit = false) {
+  if (!doit) return isPureNumber_asNumber;
+  const floats = [Math.PI, 13.234, 21.123, 34.456, 55.223];
+  const integers = [13, 21, 34, 55, 108];
+  const fns = [
+    isPureNumber_asNaN,
+    isPureNumber_asNumber,
+    isPureNumber_asParseInt,
+    isPureNumber_asParseFloat,
+    isPureNumber_asMathRound,
+    isPureNumber_asCastFloat,
+  ];
+  return fns[performanceIndexOfUnaryBool(fns, [floats, integers])];
+})(false);
 
 export function isPureString(obj) {
   return obj != null && obj.constructor === String;
@@ -29,6 +125,32 @@ export function isPureString(obj) {
 
 export function isPureObject(obj) {
   return (typeof obj === 'object' && obj.constructor !== Array);
+}
+
+export function isPureObjectReally(obj) {
+  return (typeof obj === 'object'
+    && !(obj.constructor === Array
+      || obj.constructor === Map
+      || obj.constructor === Set
+      || obj.constructor === WeakMap
+      || obj.constructor === WeakSet
+      || obj.constructor === Int8Array
+      || obj.constructor === Int16Array
+      || obj.constructor === Int32Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === BigInt64Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt8Array
+      || obj.constructor === Uint8ClampedArray
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt32Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt16Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt32Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === BigUint64Array
+    ));
 }
 
 export function isPureArray(obj) {
@@ -40,13 +162,19 @@ export function isPureTypedArray(obj) {
     && (obj.constructor === Int8Array
       || obj.constructor === Int16Array
       || obj.constructor === Int32Array
-      //|| obj.constructor === BigInt64Array
-      //|| obj.constructor === UInt8Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === BigInt64Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt8Array
       || obj.constructor === Uint8ClampedArray
-      //|| obj.constructor === UInt32Array
-      //|| obj.constructor === UInt16Array
-      //|| obj.constructor === UInt32Array
-      //|| obj.constructor === BigUint64Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt32Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt16Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === UInt32Array
+      // eslint-disable-next-line no-undef
+      || obj.constructor === BigUint64Array
     ));
 }
 
@@ -83,7 +211,7 @@ export function isStringOrObject(obj) {
       || (obj.constructor !== Array && typeof obj === 'object'));
 }
 
-export function getBoolOrNumber(obj, def) {
+export function getBoolOrNumber(obj, def = undefined) {
   return isBoolOrNumber(obj) ? obj : def;
 }
 
@@ -123,7 +251,7 @@ export function getPureNumber(obj, def) {
 }
 
 export function getPureInteger(obj, def) {
-  return (mathi32_round(obj)|0) || def;
+  return mathi32_round(obj) || def;
 }
 
 export function getPureBool(obj, def) {
