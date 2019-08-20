b5b5d045d6f002b89053f4ba0a22acdb31abd926
Joham <jklarenbeek@gmail.com>
2019-08-20 15:35:48 +0200

    changed example to object.props2

--- test/JSONSchema/index.js
+++ test/JSONSchema/index.js
@@ -8,9 +8,9 @@ import {
 } from '__futilsjs/json';
 
 import data1 from './draft7/01.type.basic1.00.json';
-import data2 from './draft7/11.combining.allOf3.01.json';
-import data2a from './draft7/11.combining.allOf3.02.true.json';
-import data2b from './draft7/11.combining.allOf3.03.false.json';
+import data2 from './draft7/05.object.props2.00.json';
+import data2a from './draft7/05.object.props2.01.true.json';
+import data2b from './draft7/05.object.props2.02.false.json';
 
 registerDefaultFormatCompilers();
 
@@ -21,7 +21,6 @@ const doc1 = getJSONSchema('01.type.basic1.00');
 console.log(doc1.baseUri, true, doc1.validate(42), doc1.errors);
 console.log(doc1.baseUri, false, doc1.validate('42'), doc1.errors);
 
-const doc2 = getJSONSchema('11.combining.allOf3.00');
-
+const doc2 = getJSONSchema('05.object.props2.00');
 console.log(doc2.baseUri, true, doc2.validate(data2a), doc2.errors);
 console.log(doc2.baseUri, false, doc2.validate(data2b), doc2.errors);
