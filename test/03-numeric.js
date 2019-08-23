6f84cb64b44f3649d54bdba703625cf51ca58196
Joham <jklarenbeek@gmail.com>
2019-08-23 15:53:37 +0200

    added mocha numeric test

---
+++ test/03-numeric.js
@@ -0,0 +1,39 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable func-names */
+/* eslint-env mocha */
+import { expect, assert } from 'chai';
+
+import {
+  compileJSONSchema,
+  getJSONSchema,
+  registerDefaultFormatCompilers,
+} from '../src/json';
+
+registerDefaultFormatCompilers();
+
+// https://json-schema.org/understanding-json-schema/reference/numeric.html
+
+describe('#numeric()', function () {
+  it('should validate numbers', function () {
+    compileJSONSchema('numberBasic1', { type: 'number' });
+
+    const root = getJSONSchema('numberBasic1');
+    assert.isTrue(root.validate(42), 'validates an integer');
+    assert.isTrue(root.validate(-1), 'validates a negative integer');
+    assert.isTrue(root.validate(Math.PI), 'validates a number');
+    assert.isTrue(root.validate(2.99792458e8), 'a floating point number');
+    assert.isFalse(root.validate('42'), 'not validates a string');
+  });
+
+  it('should validate integers', function () {
+    compileJSONSchema('numberBasic2', { type: 'integer' });
+
+    const root = getJSONSchema('numberBasic2');
+    assert.isTrue(root.validate(42), 'validates an integer');
+    assert.isTrue(root.validate(-1), 'validates a negative integer');
+    assert.isFalse(root.validate(Math.PI), 'not validates a number');
+    assert.isTrue(root.validate(2.99792458e8), 'not validates a floating point number');
+    assert.isFalse(root.validate('42'), 'not validates a string');
+  });
+
+});
