83c6c516a49198568f19c20c6337823596eae6f1
Joham <jklarenbeek@gmail.com>
2019-09-04 16:12:14 +0200

    export string compilers as array

--- src/json/compileStringValidator.js
+++ src/json/compileStringValidator.js
@@ -2,83 +2,67 @@ import {
   String_createRegExp,
 } from '../helpers/String';
 
-import { getIntegerishType } from '../types/getDataType';
+import {
+  isStrictStringType,
+} from '../types/isDataType';
+
+import {
+  getIntegerishType,
+} from '../types/getDataType';
 
 function compileMinLength(schemaObj, jsonSchema) {
   const min = Math.max(getIntegerishType(jsonSchema.minLength, 0), 0);
-  if (min > 0) {
-    const addError = schemaObj.createMemberError('minLength', min, compileStringLength);
-    return function validateStringMinLength(data) {
-      if (typeof data === 'string') {
-        const len = data.length;
-        const valid = len >= min;
-        if (!valid) addError(len);
-        return valid;
-      }
-      return true;
-    };
-  }
-  else return undefined;
+  if (min === 0) return undefined;
+
+  const addError = schemaObj.createMemberError('minLength', min, compileMinLength);
+  if (addError == null) return undefined;
+
+  return function validateStringMinLength(data) {
+    return isStrictStringType(data)
+      ? data.length >= min
+        ? true
+        : addError(data.length)
+      : true;
+  };
 }
 
 function compileMaxLength(schemaObj, jsonSchema) {
   const max = Math.max(getIntegerishType(jsonSchema.maxLength, 0), 0);
+  if (max === 0) return undefined;
 
-  if (max > 0) {
-    const addError = schemaObj.createMemberError('maxLength', max, compileStringLength);
-    return function validateStringMaxLength(data) {
-      if (typeof data === 'string') {
-        const len = data.length;
-        const valid = len <= max;
-        if (!valid) addError(len);
-        return valid;
-      }
-      return true;
-    };
-  }
-  else return undefined;
-}
+  const addError = schemaObj.createMemberError('maxLength', max, compileMaxLength);
+  if (addError == null) return undefined;
 
-function compileStringLength(schemaObj, jsonSchema) {
-  const fnMin = compileMinLength(schemaObj, jsonSchema);
-  const fnMax = compileMaxLength(schemaObj, jsonSchema);
-
-  if (fnMin && fnMax) {
-    return function validateStringBetweenLength(data, dataRoot) {
-      return fnMin(data, dataRoot) && fnMax(data, dataRoot);
-    };
-  }
-  else if (fnMin) return fnMin;
-  else if (fnMax) return fnMax;
-  else return undefined;
+  return function validateStringMaxLength(data) {
+    return isStrictStringType(data)
+      ? data.length <= max
+        ? true
+        : addError(data.length)
+      : true;
+  };
 }
 
 function compileStringPattern(schemaObj, jsonSchema) {
   const ptrn = jsonSchema.pattern;
   const re = String_createRegExp(ptrn);
-  if (re) {
-    const addError = schemaObj.createMemberError('pattern', ptrn, compileStringPattern);
-    return function validateStringPattern(data) {
-      if (typeof data === 'string') {
-        const valid = re.test(data);
-        if (!valid) addError(data);
-        return valid;
-      }
-      return true;
-    };
-  }
-  else return undefined;
+  if (re == null) return undefined;
+
+  const addError = schemaObj.createMemberError('pattern', ptrn, compileStringPattern);
+  if (addError == null) return undefined;
+
+  return function validateStringPattern(data) {
+    return isStrictStringType(data)
+      ? re.test(data)
+        ? true
+        : addError(data)
+      : true;
+  };
 }
 
 export function compileStringBasic(schemaObj, jsonSchema) {
-  const fnLength = compileStringLength(schemaObj, jsonSchema);
-  const fnPattern = compileStringPattern(schemaObj, jsonSchema);
-  if (fnLength && fnPattern) {
-    return function validateSchemaBasic(data) {
-      return fnLength(data) && fnPattern(data);
-    };
-  }
-  else if (fnLength) return fnLength;
-  else if (fnPattern) return fnPattern;
-  else return undefined;
+  return [
+    compileMinLength(schemaObj, jsonSchema),
+    compileMaxLength(schemaObj, jsonSchema),
+    compileStringPattern(schemaObj, jsonSchema),
+  ];
 }
