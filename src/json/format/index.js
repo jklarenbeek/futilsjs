dba036b5488d9fc148df1e87f7a828a03bce6cdb
Joham <jklarenbeek@gmail.com>
2019-09-10 22:33:32 +0200

    added regexFormats to default

--- src/json/format/index.js
+++ src/json/format/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 
 import { formatCompilers as dateTimeFormat } from './date-time';
+import { formatCompilers as regexFormat } from './string';
 
 export function getDefaultFormatCompilers() {
-  return { ...dateTimeFormat };
+  return { ...dateTimeFormat, ...regexFormat };
 }
